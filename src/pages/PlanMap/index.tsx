a140382c8b1950260b893c27e7d31ae093509e1d
wuhao <dpwuhao@dfl.com.cn>
2021-08-01 17:19:33 +0800

    map display ok, draw rectangle ok

--- src/pages/PlanMap/index.tsx
+++ src/pages/PlanMap/index.tsx
@@ -2,46 +2,97 @@ import { Button, Space, message, Input, Row, Col, Modal, Drawer } from 'antd';
 import React, { useState, useRef, useCallback, useEffect } from 'react';
 import { useIntl, FormattedMessage } from 'umi';
 import ProTable, { ProColumns, ActionType } from '@ant-design/pro-table';
-import { SatItem,SenItem,DataNode } from './data';
+import { SatItem, SenItem, DataNode } from './data';
 import { querySatTree } from './service';
 import { Layout, Menu, Breadcrumb, Tree } from 'antd';
 const { Header, Content, Footer, Sider } = Layout;
 import './index.css';
 import Map from 'ol/Map';
 import View from 'ol/View';
+import { defaults as defaultControls } from 'ol/control';
+import MousePosition from 'ol/control/MousePosition';
+import { createStringXY } from 'ol/coordinate';
+import { OSM, Vector as VectorSource } from 'ol/source';
+import { Tile as TileLayer, Vector as VectorLayer } from 'ol/layer';
+import Draw, {
+  createBox,
+  createRegularPolygon,
+} from 'ol/interaction/Draw';
 import Proj from 'ol/proj';
 import Source from 'ol/source';
-import TileLayer from 'ol/layer/Tile';
 import XYZ from 'ol/source/XYZ';
 
+//TODO
+/*
+1. add pan control
+2. add path layer, area layer
+3. add switch control function
+4. add path drawing
+5. add calling api to get path 
+6. add custom control to display path details
+*/
 const PlanMap: React.FC<{}> = () => {
-  const [expandedKeys, setExpandedKeys] = useState<React.Key[]>(['0-0-0', '0-0-1']);
-  const [checkedKeys, setCheckedKeys] = useState<React.Key[]>(['0-0-0']);
+  const [expandedKeys, setExpandedKeys] = useState<React.Key[]>([]);
+  const [checkedKeys, setCheckedKeys] = useState<React.Key[]>([]);
   const [selectedKeys, setSelectedKeys] = useState<React.Key[]>([]);
   const [autoExpandParent, setAutoExpandParent] = useState<boolean>(true);
   const [satTree, setSatTree] = useState<DataNode[]>()
   const refGraph = useRef<HTMLDivElement>(null)
+  const [draw, setDraw] = useState<Draw>();
+  const [map, setMap] = useState<Map>();
   const { formatMessage } = useIntl();
+
+  const mousePositionControl = new MousePosition({
+    coordinateFormat: createStringXY(4),
+    projection: 'EPSG:4326',
+    // comment the following two lines to have the mouse position
+    // be placed within the map.
+    className: 'custom-mouse-position',
+    //target: document.getElementById('mouse-position'),
+  });
+
+  const source = new VectorSource({ wrapX: false });
+
+  const vector = new VectorLayer({
+    source: source,
+  });
+
+  const addInteraction = () => {
+    if (map === undefined) { return }
+    setDraw(new Draw({
+      source: source,
+      type: "box"
+    }));
+    if (draw === undefined) { return }
+    map.addInteraction(draw);
+  }
 
   useEffect(() => {
     if (refGraph.current == null) {
       return
     }
-    new Map({
+    setMap(new Map({
       target: refGraph.current,
+      interactions: [new Draw({
+        source: source,
+        type: "Circle",
+        geometryFunction: createBox()
+      })],
       layers: [
         new TileLayer({
           source: new XYZ({
             url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png'
           })
-        })
+        }),
+        vector
       ],
       view: new View({
         center: [0, 0],
         zoom: 2
-      })
-    });
-  });
+      }),
+      controls: defaultControls().extend([mousePositionControl])
+    }));
+  }, []);
 
   const getSatTree = async () => {
     querySatTree().then(e => {
@@ -114,7 +165,7 @@ const PlanMap: React.FC<{}> = () => {
       <Layout className="site-layout">
         <Header className="site-layout-background" style={{ padding: 0 }} />
         <Content style={{ margin: '0 0px' }}>
-          <div ref={refGraph} className="map" id="map" ></div>
+          <div ref={refGraph} className="map" id="map" style={{ width: '100%', height: '500px' }}></div>
         </Content>
       </Layout>
     </Layout>
